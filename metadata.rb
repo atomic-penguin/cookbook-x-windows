843cbb06ba833262ade983c7dfe559e44cca949d
Eric G. Wolfe <wolfe21@marshall.edu>
2011-09-30 15:40:11 -0400

    Public release of Marshall University common cookbooks for Enterprise Linux

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Eric G. Wolfe"
+maintainer_email "wolfe21@marshall.edu"
+license          "Apache 2.0"
+description      "Installs x-windows for Oracle requirements"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
